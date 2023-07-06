648ca763db0d78cd3b967f98c4eb68f8a40a581b
youngkikim <rkfcl1169@gmail.com>
2023-07-06 21:59:52 +0900

    Java study

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.sound(); // 출력: Dog barks
+    }
+}
