1ed0e87274ee8465e4b50ab4535310b31c3140f5
youngkikim <rkfcl1169@gmail.com>
2023-07-06 22:04:55 +0900

    example polymorphism

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,50 @@
-class Animal {
-    void sound() {
-        System.out.println("Animal makes a sound");
+interface Weapon {
+    void attack();
+}
+
+class Sword implements Weapon {
+    public void attack() {
+        System.out.println("Swish!");
     }
 }
 
-class Dog extends Animal {
-    void sound() {
-        System.out.println("Dog barks");
+class Axe implements Weapon {
+    public void attack() {
+        System.out.println("Chop!");
+    }
+}
+
+class Bow implements Weapon {
+    public void attack() {
+        System.out.println("Twang!");
+    }
+}
+
+class Player {
+    private Weapon weapon;
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public void attack() {
+        if (weapon != null) {
+            weapon.attack();
+        }
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.sound(); // 출력: Dog barks
+        Player player = new Player();
+
+        player.setWeapon(new Sword());
+        player.attack();  // 출력: "Swish!"
+
+        player.setWeapon(new Axe());
+        player.attack();  // 출력: "Chop!"
+
+        player.setWeapon(new Bow());
+        player.attack();  // 출력: "Twang!"
     }
 }
