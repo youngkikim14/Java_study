a405d1de0e5c1049cd1ba3872d9ecfe70bf86fe6
YoungKi <rkfcl1169@gmail.com>
2023-07-31 20:13:30 +0900

    문제풀이

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,34 @@ class Dog extends Animal {
     }
 }
 
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int numer3 = numer1 * denom2;
+        int numer4 = numer2 * denom1;
+
+        int denom = denom1 * denom2;
+        int numer = numer3 + numer4;
+
+        int max = 0;
+
+        for (int i = 1; i <= denom && i <= numer; i++) {
+            if (denom % i == 0 && numer % i == 0) {
+                max = i;
+            }
+        }
+        int finalDenom = denom / max;
+        int finalNumer = numer / max;
+        int[] result = new int[1];
+        result[0] = finalNumer;
+        result[1] = finalDenom;
+        return result;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         Dog dog = new Dog();
         dog.sound(); // 출력: Dog barks
     }
 }
+
