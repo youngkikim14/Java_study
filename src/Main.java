07d926b3254aab3ce128b692c876dba28a61e302
YoungKi <rkfcl1169@gmail.com>
2023-08-01 20:12:40 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
+import java.util.Arrays;
 
 class Solution {
-        public int[] solution(int[] numbers) {
-            int[] answer = new int[numbers.length];
-            for (int i = 0; i < numbers.length; i++) {
-                answer[i] = numbers[i] * 2;
-            }
-            return answer;
+    public int solution(int[] array) {
+        Arrays.sort(array);
+        return array[array.length/2];
         }
     }
 
