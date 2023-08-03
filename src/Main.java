4786e0a3981e6bac1ece9a2c2874e974ed2e85c2
YoungKi <rkfcl1169@gmail.com>
2023-08-03 23:27:14 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,37 @@
-import java.util.Arrays;
-
 class Solution {
-    public int solution(int[] array) {
-        Arrays.sort(array);
-        return array[array.length/2];
+    public String solution(String code) {
+        String answer = "";
+//        boolean mode = false;
+        int mode = 0;
+
+        for (int i = 0; i < code.length(); i++) {
+            if(mode == 0){
+                if(code.charAt(i) != '1'){
+                    if (i % 2 ==0 ){
+                        answer += code.charAt(i);
+                    }
+                }else{
+                    mode = 1;
+                }
+            }else {
+                if(code.charAt(i) != '1'){
+                    if (i % 2 ==1){
+                        answer += code.charAt(i);
+                    }
+                }else{
+                    mode = 0;
+                }
+
+            }
+
         }
+        if(answer.isEmpty()){
+            return "EMPTY";
+        }
+
+        return answer;
     }
+}
 
 public class Main {
     public static void main(String[] args) {
