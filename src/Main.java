36fa7f9961965f7dd6fb99282def61be6f69c1c2
YoungKi <rkfcl1169@gmail.com>
2023-08-04 11:25:02 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        List<Integer> arrayList = new ArrayList<>();
+        Arrays.sort(array);
+        for (int j : array) {
+            arrayList.add(j);
+        }
+        for (int i = 1; i < arrayList.size(); i++) {
+            if (Collections.frequency(arrayList, arrayList.get(i)) <= Collections.frequency(arrayList, arrayList.get(i-1))){
+                answer = arrayList.get(i-1);
+            } else if (Collections.frequency(arrayList, arrayList.get(i)) == Collections.frequency(arrayList, arrayList.get(i-1))){
+                answer = -1;
+            } else if (array.length == 1){
+                answer = arrayList.get(0);
+            }
+        }
+        return answer;
+    }
+}
+
 
 public class Main {
     public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(new int[]{1, 1, 2, 2}));
     }
 }
 
