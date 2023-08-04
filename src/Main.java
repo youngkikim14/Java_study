4477763823a6e5b61c3d068ea9ce799a238faa7c
YoungKi <rkfcl1169@gmail.com>
2023-08-04 10:14:01 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,3 @@
-class Solution {
-    public String solution(String code) {
-        String answer = "";
-//        boolean mode = false;
-        int mode = 0;
-
-        for (int i = 0; i < code.length(); i++) {
-            if(mode == 0){
-                if(code.charAt(i) != '1'){
-                    if (i % 2 ==0 ){
-                        answer += code.charAt(i);
-                    }
-                }else{
-                    mode = 1;
-                }
-            }else {
-                if(code.charAt(i) != '1'){
-                    if (i % 2 ==1){
-                        answer += code.charAt(i);
-                    }
-                }else{
-                    mode = 0;
-                }
-
-            }
-
-        }
-        if(answer.isEmpty()){
-            return "EMPTY";
-        }
-
-        return answer;
-    }
-}
 
 public class Main {
     public static void main(String[] args) {
