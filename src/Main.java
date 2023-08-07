6f359c6ea3ce84f9d1f24d4921681d72839b3624
YoungKi <rkfcl1169@gmail.com>
2023-08-08 02:17:27 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
 class Solution {
     public int solution(int n) {
-        return n%7!=0?(n/7)+1:n/7;
+        int answer = 0;
+        int pizza = 6;
+        if (n > 3) {
+            for (int i = 1; i < 100; i++) {
+                if ((n * i) % pizza == 0) {
+                    answer = (n * i) / pizza;
+                    break;
+                }
+            }
+        } else {
+            answer = 1;
+        }
+        return answer;
     }
 }
 
 
 public class Main {
     public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(5));
     }
 }
 
