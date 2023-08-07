faa3da97a2f983238d29220358c90143a6c641c1
YoungKi <rkfcl1169@gmail.com>
2023-08-08 02:38:42 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 class Solution {
-    public int solution(int slice, int n) {
-        int answer = 0;
-        for (int i = 1; i < 100; i++) {
-            if ((slice * i) / n >= 1){
-                return i;
-            }
+    public double solution(int[] numbers) {
+        double answer = 0;
+        double sum = 0;
+        for (int number : numbers) {
+            sum += number;
         }
-        return answer;
+            return sum / (double) numbers.length;
     }
 }
 
@@ -14,7 +13,6 @@ class Solution {
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(2,1));
     }
 }
 
