1730152cda2a56d523972029029033b3824d67dc
YoungKi <rkfcl1169@gmail.com>
2023-08-07 11:34:22 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
-import java.util.ArrayList;
-
 class Solution {
-    public Integer[] solution(int n) {
-//        int[] answer = new int[n];
-        ArrayList<Integer> result = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            if (i%2 == 1){
-                result.add(i);
-            }
-        }
-        return result.toArray(result.toArray(new Integer[0]));
+    public int solution(int n) {
+        return n%7!=0?(n/7)+1:n/7;
     }
 }
 
