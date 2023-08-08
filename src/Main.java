f5e5388393a77a7d14769b912733da1c368e60a9
YoungKi <rkfcl1169@gmail.com>
2023-08-08 11:13:32 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,8 @@
 class Solution {
-    public int solution(int price) {
-        int answer = 0;
-        double discount = 0;
-        if (price >= 100000 && price < 300000){
-            discount = price * 0.05;
-        } else if (price >= 300000 && price < 500000){
-            discount = price * 0.1;
-        } else if (price >= 500000){
-            discount = price * 0.2;
-        } else {
-            return price;
-        }
-        return (int)(price-discount);
+    public int[] solution(int money) {
+        int coffee = money / 5500;
+        int change = money % 5500;
+        return new int[]{coffee, change};
     }
 }
 
