da69b34d5ef713916fbf104a86dbf5da384f3286
YoungKi <rkfcl1169@gmail.com>
2023-08-08 11:10:33 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 class Solution {
-    public double solution(int[] numbers) {
-        double answer = 0;
-        double sum = 0;
-        for (int number : numbers) {
-            sum += number;
+    public int solution(int price) {
+        int answer = 0;
+        double discount = 0;
+        if (price >= 100000 && price < 300000){
+            discount = price * 0.05;
+        } else if (price >= 300000 && price < 500000){
+            discount = price * 0.1;
+        } else if (price >= 500000){
+            discount = price * 0.2;
+        } else {
+            return price;
         }
-            return sum / (double) numbers.length;
+        return (int)(price-discount);
     }
 }
 
@@ -13,6 +19,7 @@ class Solution {
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
+        System.out.println(solution.solution(580000));
     }
 }
 
