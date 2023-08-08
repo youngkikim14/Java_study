4ba59c780a575deb79cb4bdd6564b7bd86791c99
YoungKi <rkfcl1169@gmail.com>
2023-08-08 12:28:05 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    public int solution(int age) {
-        return 2022 - age +1;
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        for (int i = num_list.length; i > 0; i--) {
+            answer[num_list.length - i] = num_list[i - 1];
+        }
+        return answer;
     }
 }
 
