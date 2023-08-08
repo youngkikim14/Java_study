d0b9ee818871f3806d70f0568ccee94ebed57878
YoungKi <rkfcl1169@gmail.com>
2023-08-08 11:14:52 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 class Solution {
-    public int[] solution(int money) {
-        int coffee = money / 5500;
-        int change = money % 5500;
-        return new int[]{coffee, change};
+    public int solution(int age) {
+        return 2022 - age +1;
     }
 }
 
@@ -10,7 +8,6 @@ class Solution {
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(580000));
     }
 }
 
