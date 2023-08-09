3f2ef2d7eea58ef141b6f2774ff61c4999210a92
YoungKi <rkfcl1169@gmail.com>
2023-08-09 21:50:28 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
+
 class Solution {
-    public int[] solution(int[] numbers, int num1, int num2) {
-        int[] answer = new int[num2 - num1 + 1];
-        for (int i = num1, j = 0; i <= num2 ; i++, j++) {
-            answer[j] = numbers[i];
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for (int a : array) {
+            if (height < a){
+                answer++;
+            }
         }
         return answer;
     }
 }
+
 public class Main {
     public static void main(String[] args) {
 
