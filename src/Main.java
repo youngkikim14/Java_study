a1027f884ccfe3520996e61aa7184a81df910bbe
YoungKi <rkfcl1169@gmail.com>
2023-08-09 11:23:46 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 class Solution {
-    public int solution(int n, int k) {
+    public int solution(int n) {
         int answer = 0;
-        answer = (12000 * n) + (2000 * k) - ((n / 10) * 2000);
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0){
+                answer += i;
+            }
+        }
         return answer;
     }
 }
