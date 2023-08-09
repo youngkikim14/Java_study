8e7570ee6eaa6dcee2d75b201f7ca6dfaa43aa19
YoungKi <rkfcl1169@gmail.com>
2023-08-09 20:57:00 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,8 @@
 class Solution {
-    public String solution(int age) {
-        String answer = "";
-        String result = Integer.toString(age);
-        for (int i = 0; i < result.length(); i++) {
-            if (result.charAt(i) == '0'){
-                answer += "a";
-            } else if (result.charAt(i) == '1'){
-                answer += "b";
-            } else if (result.charAt(i) == '2'){
-                answer += "c";
-            } else if (result.charAt(i) == '3'){
-                answer += "d";
-            } else if (result.charAt(i) == '4'){
-                answer += "e";
-            } else if (result.charAt(i) == '5'){
-                answer += "f";
-            } else if (result.charAt(i) == '6'){
-                answer += "g";
-            } else if (result.charAt(i) == '7'){
-                answer += "h";
-            } else if (result.charAt(i) == '8'){
-                answer += "i";
-            } else if (result.charAt(i) == '9'){
-                answer += "j";
-            }
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2 - num1 + 1];
+        for (int i = num1, j = 0; i <= num2 ; i++, j++) {
+            answer[j] = numbers[i];
         }
         return answer;
     }
