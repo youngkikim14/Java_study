3680895ddcdd4b6ada4456bde0918cbd93407b61
YoungKi <rkfcl1169@gmail.com>
2023-08-09 14:21:49 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0){
-                answer += i;
+    public String solution(int age) {
+        String answer = "";
+        String result = Integer.toString(age);
+        for (int i = 0; i < result.length(); i++) {
+            if (result.charAt(i) == '0'){
+                answer += "a";
+            } else if (result.charAt(i) == '1'){
+                answer += "b";
+            } else if (result.charAt(i) == '2'){
+                answer += "c";
+            } else if (result.charAt(i) == '3'){
+                answer += "d";
+            } else if (result.charAt(i) == '4'){
+                answer += "e";
+            } else if (result.charAt(i) == '5'){
+                answer += "f";
+            } else if (result.charAt(i) == '6'){
+                answer += "g";
+            } else if (result.charAt(i) == '7'){
+                answer += "h";
+            } else if (result.charAt(i) == '8'){
+                answer += "i";
+            } else if (result.charAt(i) == '9'){
+                answer += "j";
             }
         }
         return answer;
