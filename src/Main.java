13aedcb1eac05924955aacb34b192648a1282e2e
YoungKi <rkfcl1169@gmail.com>
2023-08-10 10:56:31 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    public String solution(String letter) {
+    public String solution(String rsp) {
         String answer = "";
-        String[] result = letter.split(" ");
-        Map<String, String> m = new HashMap<>();
-        m.put(".-","a");m.put("-...","b");m.put("-.-.","c");m.put("-..","d");m.put(".","e");m.put("..-.","f");m.put("--.","g");m.put("....","h");m.put("..","i");m.put(".---","j");m.put("-.-","k");m.put(".-..","l");m.put("--","m");m.put("-.","n");m.put("---","o");m.put(".--.","p");m.put("--.-","q");m.put(".-.","r");m.put("...","s");m.put("-","t");m.put("..-","u");m.put("...-","v");m.put(".--","w");m.put("-..-","x");m.put("-.--","y");m.put("--..","z");
-        for (String a : result) {
-            answer += m.get(a);
+        char[] game = rsp.toCharArray();
+        for (char a : game) {
+            if (a == '0'){
+                answer += "5";
+            } else if (a == '2'){
+                answer += "0";
+            } else if (a == '5'){
+                answer += "2";
+            }
         }
         return answer;
     }
