95c974c9e0a07dd1271d7f261637d7248975ee43
YoungKi <rkfcl1169@gmail.com>
2023-08-10 10:51:59 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    public int solution(int hp) {
-        int answer = 0;
-        int general = 0;
-        int soldier = 0;
-        int worker = 0;
-        while (hp != 0){
-            general = hp / 5;
-            hp -= general * 5;
-            soldier = hp / 3;
-            hp -= soldier * 3;
-            worker = hp;
-            hp -= worker;
-            answer = general + soldier + worker;
+    public String solution(String letter) {
+        String answer = "";
+        String[] result = letter.split(" ");
+        Map<String, String> m = new HashMap<>();
+        m.put(".-","a");m.put("-...","b");m.put("-.-.","c");m.put("-..","d");m.put(".","e");m.put("..-.","f");m.put("--.","g");m.put("....","h");m.put("..","i");m.put(".---","j");m.put("-.-","k");m.put(".-..","l");m.put("--","m");m.put("-.","n");m.put("---","o");m.put(".--.","p");m.put("--.-","q");m.put(".-.","r");m.put("...","s");m.put("-","t");m.put("..-","u");m.put("...-","v");m.put(".--","w");m.put("-..-","x");m.put("-.--","y");m.put("--..","z");
+        for (String a : result) {
+            answer += m.get(a);
         }
         return answer;
     }
