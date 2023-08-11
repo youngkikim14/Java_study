dc95c0cc3c330769e1b5b3dc252fcc2303276c25
YoungKi <rkfcl1169@gmail.com>
2023-08-11 11:15:29 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 class Solution {
-    public int[][] solution(int[] num_list, int n) {
-        int[][] answer = new int[num_list.length/n][n];
-        int index=0;
-        for(int i=0; i< answer.length; i++) {
-            int[] tmp = new int[n];
-            for(int k=0; k<n; k++) {
-                tmp[k] = num_list[index];
-                index++;
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        int a = 0;
+        int i = 0;
+        while (a < k){
+            if (i == numbers.length){
+                i = 0;
+            } else if (i == numbers.length+1){
+                i = 1;
             }
-            answer[i] = tmp;
+            answer = numbers[i];
+            i+=2;
+            a++;
         }
         return answer;
     }
@@ -16,7 +19,9 @@ class Solution {
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        int[] a = {1, 2, 3};
+        System.out.println(solution.solution(a, 3));
     }
 }
 
