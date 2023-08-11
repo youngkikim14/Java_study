a2efe6a3d00233fa5e4422697cc8efd7c5047329
YoungKi <rkfcl1169@gmail.com>
2023-08-11 22:41:16 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
 class Solution {
-    public int solution(int[] box, int n) {
-        return (box[0]/n) * (box[1]/n) * (box[2]/n);
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            int a = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0){
+                    a++;
+                    if (a == 3){
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
+        System.out.println(solution.solution(10));
     }
 }
 
