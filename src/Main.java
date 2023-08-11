e599c67b3bdd6871f8a0db405827a34f4650cee7
YoungKi <rkfcl1169@gmail.com>
2023-08-11 22:26:25 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,6 @@
-
-
 class Solution {
-    public int[] solution(int[] numbers, String direction) {
-        int[] answer = new int[numbers.length];
-            if (direction.equals("right")){
-                for (int i = 1; i < numbers.length; i++) {
-                answer[0] = numbers[numbers.length-1];
-                answer[i] = numbers[i-1];
-            }
-        } else if (direction.equals("left")){
-                for (int i = 0; i < numbers.length-1; i++) {
-                    answer[numbers.length-1] = numbers[0];
-                    answer[i] = numbers[i+1];
-                }
-            }
-        return answer;
+    public int solution(int[] box, int n) {
+        return (box[0]/n) * (box[1]/n) * (box[2]/n);
     }
 }
 
