9df47d519f1ccca074396aff185c604001646c1f
YoungKi <rkfcl1169@gmail.com>
2023-08-14 23:37:32 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,7 @@
-import java.util.Arrays;
 
-class Solution {
-    public int solution(int[] numbers) {
-        int answer = 0;
-        Arrays.sort(numbers);
-        answer = numbers[numbers.length - 1] * numbers[numbers.length - 2];
-        return answer;
-    }
-}
 
 public class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.solution(10));
     }
 }
 
