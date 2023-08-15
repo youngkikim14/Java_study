b63f0b6338510eecf7f34b422459316f10b37b25
YoungKi <rkfcl1169@gmail.com>
2023-08-15 11:07:32 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 class Solution {
-    public String solution(String my_string) {
-        return my_string.replaceAll("[aieou]", "");
+    public int[] solution(String my_string) {
+        String number = my_string.replaceAll("[a-z]", "");
+        int result = Integer.parseInt(number);
+        int[] answer = Stream.of(String.valueOf(result).split(""))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        Arrays.sort(answer);
+        return answer;
     }
 }
 
