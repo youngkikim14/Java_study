07c843c5c3f34b769383b1d0e615bc15ef3a5336
YoungKi <rkfcl1169@gmail.com>
2023-08-15 10:59:43 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,6 @@
 class Solution {
-    public int solution(int n) {
-        int answer = 1;
-        int result = 1;
-        while (answer < 10){
-            result *= answer;
-            if (result == n){
-                break;
-            } else if (result  > n){
-                answer--;
-                break;
-            }
-            answer++;
-        }
-        return answer;
+    public String solution(String my_string) {
+        return my_string.replaceAll("[aieou]", "");
     }
 }
 
