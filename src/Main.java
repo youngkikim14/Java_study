659342a91c39eeec444e924c6b2931253de7089e
YoungKi <rkfcl1169@gmail.com>
2023-08-15 10:39:22 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
-
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        int result = 1;
+        while (answer < 10){
+            result *= answer;
+            if (result == n){
+                break;
+            } else if (result  > n){
+                answer--;
+                break;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
