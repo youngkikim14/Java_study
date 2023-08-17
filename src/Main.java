1429ed90556fb22dc1d4eab3586aac58545aa421
YoungKi <rkfcl1169@gmail.com>
2023-08-17 16:06:15 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,10 @@
-import java.util.Arrays;
-
 class Solution {
-    public int solution(String s) {
-        String[] a = s.split(" ");
-        int[] result = new int[a.length];
-        for (int i = 0; i < a.length; i++) {
-            if (a[i].equals("Z")){
-                a[i - 1] = "0";
-                a[i] = "0";
-            }
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        for (int i = 0; i < strlist.length; i++) {
+            answer[i] = strlist[i].length();
         }
-        for (int i = 0; i < a.length; i++) {
-            result[i] = Integer.parseInt(a[i]);
-        }
-        return Arrays.stream(result).sum();
+        return answer;
     }
 }
 
