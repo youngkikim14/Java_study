3ea3b17bd5a075901bb9c25b1ae357de7cf60846
YoungKi <rkfcl1169@gmail.com>
2023-08-17 11:06:18 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,27 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int n) {
-        int[] answer = {};
-        Set<Integer> result = new HashSet<>();
-        int a = 0;
-        for (int i = 2; i < n; i++) {
-            if (n % i ==0){
-                a = n / i;
-                while (a % 2 == 0){
-                    a /= 2;
-                    result.add(a);
+        List<Integer> list = new ArrayList<>();
+
+        //자연수 n의 소인수를 구하기 위한 반복문
+        for(int i = 2; i <= n; i++) {
+            /*나누어 떨어지면 i는 소인수,
+            몫이 2이상인 경우도 있기 때문에 안 나눠질 때까지 나눠줌*/
+            if(n % i == 0) {
+                while(n % i == 0) {
+                    n /= i;
                 }
+                list.add(i); //구한 소인수를 list에 넣어줌
             }
         }
-        return answer;
+        int[] answer = new int[list.size()]; //소인수 갯수와 같은 길이로 설정
+        //list의 값들을 answer 배열에 넣어줌
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer; //소인수 배열 return
     }
 }
 
