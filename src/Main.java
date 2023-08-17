e992000f1ec119e9ed58cea81d4f6a29465c17f4
YoungKi <rkfcl1169@gmail.com>
2023-08-17 17:30:41 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 class Solution {
-    public int[] solution(String[] strlist) {
-        int[] answer = new int[strlist.length];
-        for (int i = 0; i < strlist.length; i++) {
-            answer[i] = strlist[i].length();
+    public static String solution(String str) {
+        String answer = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if(str.indexOf(str.charAt(i)) == i) answer += str.charAt(i);
         }
+
         return answer;
     }
 }
 
-public class Main {
-    public static void main(String[] args) {
-    }
+    public class Main {
+        public static void main(String[] args) {
+        }
 }
+
 
