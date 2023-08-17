f7d81cb9b070e7b3472eeb549477bd5fc47dfa0a
YoungKi <rkfcl1169@gmail.com>
2023-08-17 11:37:56 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,19 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
-    public int[] solution(int n) {
-        List<Integer> list = new ArrayList<>();
-
-        //자연수 n의 소인수를 구하기 위한 반복문
-        for(int i = 2; i <= n; i++) {
-            /*나누어 떨어지면 i는 소인수,
-            몫이 2이상인 경우도 있기 때문에 안 나눠질 때까지 나눠줌*/
-            if(n % i == 0) {
-                while(n % i == 0) {
-                    n /= i;
-                }
-                list.add(i); //구한 소인수를 list에 넣어줌
+    public int solution(String s) {
+        String[] a = s.split(" ");
+        int[] result = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i].equals("Z")){
+                a[i - 1] = "0";
+                a[i] = "0";
             }
         }
-        int[] answer = new int[list.size()]; //소인수 갯수와 같은 길이로 설정
-        //list의 값들을 answer 배열에 넣어줌
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
+        for (int i = 0; i < a.length; i++) {
+            result[i] = Integer.parseInt(a[i]);
         }
-        return answer; //소인수 배열 return
+        return Arrays.stream(result).sum();
     }
 }
 
