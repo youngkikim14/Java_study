904ffeadc6c09a486a07394038fe178a00c846f5
YoungKi <rkfcl1169@gmail.com>
2023-08-19 03:42:21 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
-
 class Solution {
-    public String solution(String my_string) {
-        String answer = "";
-        for (int i = 0; i < my_string.length(); i++) {
-            if(Character.isLowerCase(my_string.charAt(i))){
-                answer += Character.toUpperCase(my_string.charAt(i));
-            } else {
-                answer += Character.toLowerCase(my_string.charAt(i));
-            }
-        }
+    public long solution(String numbers) {
+        long answer = 0;
+        numbers = numbers.replace("zero", "0");
+        numbers = numbers.replace("one", "1");
+        numbers = numbers.replace("two", "2");
+        numbers = numbers.replace("three", "3");
+        numbers = numbers.replace("four", "4");
+        numbers = numbers.replace("five", "5");
+        numbers = numbers.replace("six", "6");
+        numbers = numbers.replace("seven", "7");
+        numbers = numbers.replace("eight", "8");
+        numbers = numbers.replace("nine", "9");
+        answer = Long.parseLong(numbers);
         return answer;
     }
 }
@@ -16,7 +19,7 @@ class Solution {
     public class Main {
         public static void main(String[] args) {
             Solution solution = new Solution();
-            System.out.println(solution.solution("cccCCC"));
+            System.out.println(solution.solution();
         }
 }
 
