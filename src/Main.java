989a11b462663b72e15bcfe7fa1a2906033b9c90
YoungKi <rkfcl1169@gmail.com>
2023-08-18 11:00:11 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,29 @@
 import java.util.Arrays;
 
 class Solution {
-    public int solution(int[] sides) {
-        Arrays.sort(sides);
-        if (sides[0]+sides[1] > sides[2]){
-            return 1;
-        } else return 2;
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        Arrays.sort(array);
+        int[] result = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = Math.abs(n - array[i]);
+        }
+        int min = result[0];
+        int minIndex = 0;
+
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] < min) {
+                min = result[i];
+                minIndex = i;
+            }
+        }
+        return array[minIndex];
     }
 }
 
     public class Main {
         public static void main(String[] args) {
+            Solution solution = new Solution();
         }
 }
 
