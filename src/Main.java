776d9d1d6b7525cbbb284359e784891fd9c7a62f
YoungKi <rkfcl1169@gmail.com>
2023-08-19 04:01:14 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 class Solution {
-    public long solution(String numbers) {
-        long answer = 0;
-        numbers = numbers.replace("zero", "0");
-        numbers = numbers.replace("one", "1");
-        numbers = numbers.replace("two", "2");
-        numbers = numbers.replace("three", "3");
-        numbers = numbers.replace("four", "4");
-        numbers = numbers.replace("five", "5");
-        numbers = numbers.replace("six", "6");
-        numbers = numbers.replace("seven", "7");
-        numbers = numbers.replace("eight", "8");
-        numbers = numbers.replace("nine", "9");
-        answer = Long.parseLong(numbers);
-        return answer;
+    public String solution(String my_string, int num1, int num2) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < my_string.length(); i++) {
+            if(i == num1){
+                answer.append(my_string.charAt(num2));
+            } else if (i == num2){
+                answer.append(my_string.charAt(num1));
+            } else {
+                answer.append(my_string.charAt(i));
+            }
+        }
+        return answer.toString();
     }
 }
 
