606c57bf0a47cd78d289fe70559840bd86531f80
YoungKi <rkfcl1169@gmail.com>
2023-08-20 22:23:02 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,35 @@
+import java.util.Arrays;
+
 class Solution {
-    public String solution(String my_string, int num1, int num2) {
+    public String solution(String s) {
+        if (s.length() == 1){
+            return s;
+        }
         StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < my_string.length(); i++) {
-            if(i == num1){
-                answer.append(my_string.charAt(num2));
-            } else if (i == num2){
-                answer.append(my_string.charAt(num1));
-            } else {
-                answer.append(my_string.charAt(i));
+        char[] result = s.toCharArray();
+        Arrays.sort(result);
+        // aaabbcdd
+        for (int i = 1; i < result.length-1; i++) {
+            if(result[i-1] != result[i] && result[i] != result[i+1]){
+                answer.append(result[i]);
             }
         }
-        return answer.toString();
+        if (result[result.length-2] != result[result.length-1]){
+            answer.append(result[result.length - 1]);
+        }
+        if (result[0] != result[1]){
+            answer.append(result[0]);
+        }
+        char[] a = answer.toString().toCharArray();
+        Arrays.sort(a);
+        return new String(a);
     }
 }
 
     public class Main {
         public static void main(String[] args) {
             Solution solution = new Solution();
-            System.out.println(solution.solution();
+            System.out.println(solution.solution("a"));
         }
 }
 
