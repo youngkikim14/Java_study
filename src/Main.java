1948dc001d486b984c5f00fc8f55e6427bab6a0c
YoungKi <rkfcl1169@gmail.com>
2023-08-20 23:13:04 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    public int[] solution(int n) {
-        int[] answer = {};
-        List<Integer> result = new ArrayList<>();
-        for (int i = 1; i < n+1; i++) {
-            if (n % i ==0){
-                result.add(i);
-            }
-        }
-        answer = result.stream().mapToInt(Integer::intValue).toArray();
-        return answer;
+    public int solution(String message) {
+        return message.length()*2;
     }
 }
 
