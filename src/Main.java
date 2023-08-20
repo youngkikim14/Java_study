f1f053b7b2e04490e5659a300fc008c18362987a
YoungKi <rkfcl1169@gmail.com>
2023-08-20 22:52:28 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,17 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    public String solution(String s) {
-        if (s.length() == 1){
-            return s;
-        }
-        StringBuilder answer = new StringBuilder();
-        char[] result = s.toCharArray();
-        Arrays.sort(result);
-        // aaabbcdd
-        for (int i = 1; i < result.length-1; i++) {
-            if(result[i-1] != result[i] && result[i] != result[i+1]){
-                answer.append(result[i]);
+    public int[] solution(int n) {
+        int[] answer = {};
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i < n+1; i++) {
+            if (n % i ==0){
+                result.add(i);
             }
         }
-        if (result[result.length-2] != result[result.length-1]){
-            answer.append(result[result.length - 1]);
-        }
-        if (result[0] != result[1]){
-            answer.append(result[0]);
-        }
-        char[] a = answer.toString().toCharArray();
-        Arrays.sort(a);
-        return new String(a);
+        answer = result.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
     }
 }
 
