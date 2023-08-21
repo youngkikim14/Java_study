42873a9ede1797afc51c80b05ccb41be23bf9793
YoungKi <rkfcl1169@gmail.com>
2023-08-21 16:49:51 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
 class Solution {
-    public int solution(String[] s1, String[] s2) {
+    public int solution(int num, int k) {
         int answer = 0;
-        for (int i = 0; i < s1.length; i++) {
-            for (int j = 0; j < s2.length; j++) {
-                if (s1[i].equals(s2[j])){
-                    answer++;
-                }
+        int result = 0;
+        int total = 0;
+        while (num % 10 != 0){
+            result = num % 10;
+            num = num / 10;
+            total++;
+            if (result == k){
+                answer = total;
             }
         }
-        return answer;
+        return answer == 0 ? -1 : total - answer+1;
     }
 }
 
     public class Main {
         public static void main(String[] args) {
             Solution solution = new Solution();
-            System.out.println(solution.solution());
+            System.out.println(solution.solution(10001, 0));
         }
 }
 
