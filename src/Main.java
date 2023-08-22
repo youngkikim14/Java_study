3b4afff920d9646ab2643285a295b614899ccd66
YoungKi <rkfcl1169@gmail.com>
2023-08-22 12:49:16 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,28 @@
-import java.util.*;
-import java.util.stream.Collectors;
 class Solution {
-    public static int[] solution(int N, int[] stages) {
-        Arrays.sort(stages);
-        List<Integer> list = Arrays.stream(stages).boxed().collect(Collectors.toList());
-        int[] c = new int [N];
-        int[] answer = new int[N];
-        Map<Integer,Float> result= new HashMap<>();
-        int a = stages.length;
-        for(int i = 0 ; i < N; i++){
-            c[i] = Collections.frequency(list,i+1);
-//            System.out.println(c.length);
-            if(a==0){
-                result.put((i+1), 0.0F);
-            }else{
-                result.put((i+1), ((float) c[i] / a));
+    public static String[] solution(int n, int[] arr1, int[] arr2) {
+        // 1. 받은 정수를 2진수 변환하여 담을 변수 선언
+        String [] binary1 = new String[arr1.length];
+        String [] binary2 = new String[arr2.length];
+        String [] answer = new String[n];
+        for( int i = 0 ; i<n ;i ++){
+            String s = String.valueOf(n);
+            binary1[i]= String.format("%" + n + "s" ,Integer.toBinaryString(arr1[i])).replace(" ","0");
+            binary2[i]= String.format("%" + n + "s" ,Integer.toBinaryString(arr2[i])).replace(" ","0");
+        }
+        for( int i = 0 ; i<n ;i ++){
+            String result = "";
+            for(int j = 0 ; j < n; j ++){
+                if(binary1[i].charAt(j)=='1' || binary2[i].charAt(j)=='1'){
+                    result += "#";
+                }else {
+                    result += " ";
+                }
             }
-            // System.out.println("실패율" + result.values());
-            a = a - c[i];
+            answer[i] = result;
         }
-        Map<Integer, Float> sortedByValueDesc = result.entrySet()
-                .stream()
-                .sorted(Map.Entry.<Integer, Float>comparingByValue().reversed())
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (e1, e2) -> e1,
-                        LinkedHashMap::new
-                ));
-        int i =0;
-        for (int b : sortedByValueDesc.keySet()){
-            answer[i]= b;
-            // System.out.println(answer[i]);
-            i++;
-        }
+//        System.out.println("binary1 = " + Arrays.toString(binary1));
+//        System.out.println("result = " + Arrays.toString(answer));
+//        System.out.println("binary2 = " + Arrays.toString(binary2));
         return answer;
     }
 }
@@ -41,7 +30,7 @@ class Solution {
     public class Main {
         public static void main(String[] args) {
             Solution solution = new Solution();
-            System.out.println(solution.solution(10001, 0));
+            System.out.println(solution.solution());
         }
 }
 
