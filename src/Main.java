e99c5e7148dd57f7a79a6f543b5e0a244a210194
YoungKi <rkfcl1169@gmail.com>
2023-08-23 15:11:42 +0900

    알고리즘 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -15,17 +15,66 @@ class Solution {
                 String oh = "happy!";
             } else result1.add(result[i]);
         }
-//        System.out.println(Arrays.toString(result1.toArray()));
+        System.out.println(Arrays.toString(result1.toArray()));
+        String [][] result2 = new String[3][];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < result2.length; j++) {
 
-
+            }
+        }
         return answer;
     }
+//public static int solution(String dartResult) {
+//    int answer = 0;
+//    int[] anwer1 = new int[3];
+//    //문자열을 나눈다
+//    String[] result = dartResult.split("(?=(\\d))");
+//    Character[][] secondArr = new Character[result.length][];
+//    for (int i = 0; i < result.length; i++) {
+//        secondArr[i] = new Character[result[i].length()];
+//        for (int j = 0; j < result[i].length(); j++) {
+//            secondArr[i][j] = result[i].charAt(j);
+//        }
+//    }
+//    for (int i = 0; i < result.length; i++) {
+//        for (int j = 0; j < secondArr[i].length; j++) {
+//            double a = 0;
+//            a = (int) (secondArr[i][0] - '0');
+//            if (secondArr[i][0] == '1' && secondArr[i][1] == '0') {
+//                a = 10;
+//                j++;
+//            }
+//            if (secondArr[i][j] == 'S') {
+//                a = Math.pow(a, 1);
+//                anwer1[i] += a;
+//            } else if (secondArr[i][j] == 'D') {
+//                a = Math.pow(a, 2);
+//                anwer1[i] += a;
+//            } else if (secondArr[i][j] == 'T') {
+//                a = Math.pow(a, 3);
+//                anwer1[i] += a;
+//            } else if (secondArr[i][j] == '*') {
+//                anwer1[i] = anwer1[i] * 2;
+//                if (i != 0) {
+//                    anwer1[i - 1] = anwer1[i - 1] * 2;
+//                }
+//            } else if (secondArr[i][j] == '#') {
+//                anwer1[i] = anwer1[i] * (-1);
+//            }
+//        }
+//    }
+//    System.out.println(Arrays.toString(result));
+//    System.out.println(Arrays.toString(anwer1));
+//    System.out.println(Arrays.stream(anwer1).sum());
+////        System.out.println(Arrays.deepToString(secondArr));
+//    return Arrays.stream(anwer1).sum();
+//    }
 }
 
     public class Main {
         public static void main(String[] args) {
             Solution solution = new Solution();
-            System.out.println(solution.solution("1S*2T*3S"));
+            System.out.println(solution.solution("10S*2T*10S"));
         }
 }
 
